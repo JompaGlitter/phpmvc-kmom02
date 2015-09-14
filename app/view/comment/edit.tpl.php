2429f877a012f67cda981c0ada2b6c6438fa5328
Jon Fredelius <jonfredelius@gmail.com>
2015-09-14 20:24:43 +0200

    removed the auto http:// input in the edit form web field

--- app/view/comment/edit.tpl.php
+++ app/view/comment/edit.tpl.php
@@ -6,7 +6,7 @@
         <legend>Ändra kommentaren</legend>
         <p><label>Name:<br/><input type='text' name='name' value='<?=$comment['name']?>'/></label></p>
         <p><label>Text:<br/><textarea name='content'><?=$comment['content']?></textarea></label></p>
-        <p><label>Hemsida:<br/><input type='text' name='web' value='http://<?=$comment['web']?>'/></label></p>
+        <p><label>Hemsida:<br/><input type='text' name='web' value='<?=$comment['web']?>'/></label></p>
         <p><label>Email:<br/><input type='text' name='mail' value='<?=$comment['mail']?>'/></label></p>
         <p class=buttons>
             <input type='submit' name='doEdit' value='Uppdatera' onClick="this.form.action = '<?=$this->url->create('comment/edit/' . $id . '/' . $pagekey)?>'"/>
